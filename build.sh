18442e0397873ce54f4a3ee069330adc5bd3adcb
literming <literming@qq.com>
2023-04-12 23:26:52 +0800

    visionfive2: Add build script file

---
+++ build.sh
@@ -0,0 +1,21 @@
+
+DEPLOY_PATH=/root
+ETHTOOL_PATH=$DEPLOY_PATH/ethtool
+LMNL_INSTALL_PATH=$DEPLOY_PATH/libmnl-1.0.5/install_path
+'''
+cd $DEPLOY_PATH
+wget --no-check-certificate  https://www.netfilter.org/pub/libmnl/libmnl-1.0.5.tar.bz2
+tar -jxvf libmnl-1.0.5.tar.bz2 libmnl-1.0.5/
+cd libmnl-1.0.5/
+mkdir install_path
+./configure --prefix=$LMNL_INSTALL_PATH LDFLAGS=-static
+make && make install
+'''
+cd $ETHTOOL_PATH
+./configure MNL_CFLAGS="-I $LMNL_INSTALL_PATH/include" MNL_LIBS="-L $LMNL_INSTALL_PATH/lib -lmnl" LDFLAGS=-static
+if [ -f ethtool ]; then
+	mv ethtool ethtool_bak 
+fi
+make
+
+
